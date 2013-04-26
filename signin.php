f8eb6a3e7c8a904103f77af9463aa9320716e72b
Chris Cartland <c.cartland@gmail.com>
2013-04-26 13:59:49 -0700

    Remove tokeninfo check. $client->authenticate($code) will only succeed if code is valid for the current app.

--- signin.php
+++ signin.php
@@ -96,34 +96,9 @@ $app->post('/connect', function (Request $request) use($app, $client,
       //$app['session']->set('state', '');
 
       $code = $request->getContent();
-      $gPlusId = $request->get['gplus_id'];
       // Exchange the OAuth 2.0 authorization code for user credentials.
       $client->authenticate($code);
-
       $token = json_decode($client->getAccessToken());
-      //verify the token
-      $reqUrl = 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=' .
-              $token->access_token;
-      $req = new Google_HttpRequest($reqUrl);
-
-      $tokenInfo = json_decode(
-          $client::getIo()->authenticatedRequest($req)
-              ->getResponseBody());
-
-      // If there was an error in the token info, abort.
-      if ($tokenInfo->error) {
-        return new Response($tokenInfo->error, 401);
-      }
-      // Make sure the token we got is for the intended user.
-      if ($tokenInfo->userid != $gPlusId) {
-        return new Response(
-            'Token\'s user ID doesn\'t match given user ID', 401);
-      }
-      // Make sure the token we got is for our app.
-      if ($tokenInfo->audience != CLIENT_ID) {
-        return new Response(
-            'Token\'s client ID does not match app\'s.', 401);
-      }
 
       // Store the token in the session for later use.
       $app['session']->set('token', json_encode($token));
